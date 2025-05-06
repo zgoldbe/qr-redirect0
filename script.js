cd172c672efe6bedf50b3f5c198600ce4a9e645d
zgoldbe <zgoldbe@toronto.ca>
2025-05-05 23:11:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("staff-link").href = PORTAL_LINKS.staff;
-  document.getElementById("guest-link").href = PORTAL_LINKS.guest;
-
   const params = new URLSearchParams(window.location.search);
   const qrCode = params.get('qr');
+  const staffLink = document.getElementById("staff-link");
+  const guestLink = document.getElementById("guest-link");
+
   if (qrCode) {
     document.getElementById("qr-code").textContent = `#${qrCode}`;
+    staffLink.href = `${PORTAL_LINKS.staff}?qr=${encodeURIComponent(qrCode)}`;
+    guestLink.href = `${PORTAL_LINKS.guest}?qr=${encodeURIComponent(qrCode)}`;
+  } else {
+    staffLink.href = PORTAL_LINKS.staff;
+    guestLink.href = PORTAL_LINKS.guest;
   }
 });
