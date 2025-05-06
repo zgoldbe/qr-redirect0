11b823815f6ad57bc5cddf0fdd5169417bf08149
cot-redirect <zgoldbe@toronto.ca>
2025-05-06 00:19:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const PORTAL_LINKS = {
-  staff: "https://example.com/staff",
+  staff: "https://cotsurvey.chkmkt.com/?e=445389&d=e&h=D2EA20243A6D94E&l=en",
   guest: "https://example.com/guest"
 };
